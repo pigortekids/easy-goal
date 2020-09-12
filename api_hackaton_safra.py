465ba767ccc46d799f8fa86600a601a3744de000
Igor Correa <igor.a.correa@hotmail.com>
2020-09-12 00:38:40 -0300

    progresso dia 2020-09-12

---
+++ api_hackaton_safra.py
@@ -0,0 +1,117 @@
+import os
+from datetime import datetime, timedelta
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+# cria uma chamada padrao de API
+def api_request( host, end_point, request_type, headers, body=None, correct_response=None, error_response='Error' ):
+    retur = False
+    url = host + end_point
+
+    response = None
+    if request_type == 'GET':
+        response = requests.get( url=url, data=body, headers=headers )
+    elif request_type == 'POST':
+        response = requests.post( url=url, data=body, headers=headers )
+    
+    status_code = response.status_code
+    output = None
+    try:
+        output = response.json()
+    except ValueError:
+        output = response.text
+    
+    if status_code == 200:
+        """
+        if correct_response != None:
+            print( '{0} -> {1}'.format( datetime.now(), correct_response ) )
+        if output != None and output != '':
+            print( '{0} -> {1}'.format( datetime.now(), output ) )
+        """
+        retur = True
+    else:
+        print( '{0} -> {1} {2}'.format( datetime.now(), error_response, status_code ) )
+        print( '{0} -> Erro com descricao "{1}"'.format( datetime.now(), output ) )
+    
+    return retur, output
+
+def use_api( accountId, tipo_chamada_api, infos=None ):
+    # pega diretorio do script
+    script_folder = os.path.dirname(os.path.realpath(__file__)) + '\\'
+
+    # leitura do arquivo com token e data de expiracao
+    with open( script_folder + 'token.txt', 'r' ) as f:
+        auth_token = f.readline().replace('\n', '')
+        horario_exp_str = f.readline()
+
+    # transforma a data em string para datetime
+    horario_exp = datetime.strptime(horario_exp_str, '%Y-%m-%d %H:%M:%S.%f')
+
+    # caso o token expirou, renova o token
+    if horario_exp <= datetime.now():
+        auth = HTTPBasicAuth('e33b611a81204f318a15d5728b998661', '31591ad2-1cbd-44e9-a01c-1bbcc0985544')
+        url = 'https://idcs-902a944ff6854c5fbe94750e48d66be5.identity.oraclecloud.com/oauth2/v1/token'
+        body = 'grant_type=client_credentials&scope=urn:opc:resource:consumer::all'
+        headers = {'Content-Type':'application/x-www-form-urlencoded'}
+
+        response = requests.post( url=url, auth=auth, data=body, headers=headers )
+
+        if response.status_code == 200:
+            auth_token = response.json()['access_token']
+            expiracao = response.json()['expires_in']
+            with open( script_folder + 'token.txt', 'w' ) as f:
+                f.write( auth_token + '\n' )
+                f.write( str(datetime.now() + timedelta( seconds=expiracao )) )
+            print( '{0} -> Token atualizado'.format( datetime.now() ) )
+        else:
+            print( '{0} -> Erro com codigo {1}'.format( datetime.now(), response.status_code ) )
+            print( '{0} -> Erro com descricao "{1}"'.format( datetime.now(), response.json() ) )
+
+    # variaveis
+    host = 'https://af3tqle6wgdocsdirzlfrq7w5m.apigateway.sa-saopaulo-1.oci.customer-oci.com/fiap-sandbox/'
+    headers = {'Authorization': 'Bearer ' + auth_token}
+
+    #confere se a API esta funcionando
+    end_point = 'health'
+    working = api_request( host=host, end_point=end_point, request_type='GET', headers=headers, error_response='API fora do ar' )[0]
+    if working:
+
+        # conta
+        if tipo_chamada_api == 'conta':
+            end_point = 'open-banking/v1/accounts/{0}'.format( accountId )
+            response = api_request( host=host, end_point=end_point, request_type='GET', headers=headers, correct_response='CONTA' )
+            return response[1]['Data']['Account'][0]
+
+        # saldo
+        if tipo_chamada_api == 'saldo':
+            end_point = 'open-banking/v1/accounts/{0}/balances'.format( accountId )
+            response = api_request( host=host, end_point=end_point, request_type='GET', headers=headers, correct_response='SALDO' )
+            return response[1]['Data']['Balance'][0]['Amount']['Amount']
+
+        # extrato
+        if tipo_chamada_api == 'extrato':
+            end_point = 'open-banking/v1/accounts/{0}/transactions'.format( accountId )
+            response = api_request( host=host, end_point=end_point, request_type='GET', headers=headers, correct_response='EXTRATO' )
+            return response[1]['data']['transaction']
+
+        # transferencia
+        if tipo_chamada_api == 'trans':
+            end_point = 'accounts/v1/accounts/{0}/transfers'.format( accountId )
+            api_request( host=host, end_point=end_point, request_type='GET', headers=headers, correct_response='TRANSFERENCIA' )
+
+        # opt in (erro 415)
+        if tipo_chamada_api == 'opt_in':
+            end_point = 'accounts/v1/optin'
+            response = api_request( host=host, end_point=end_point, request_type='POST', headers=headers, body=infos, correct_response='TRANSFERENCIA' )
+            return response
+
+        # morning calls
+        if tipo_chamada_api == 'calls':
+            filtro = 'fromData=2020-07-09&2020-07-14&playlist=morningCalls&channel=safra'
+            end_point = '/media/v1/youtube?{0}'.format( filtro )
+            api_request( host=host, end_point=end_point, request_type='GET', headers=headers, correct_response='OPT IN' )
+
+    # qual o sentido de eu ver se a API ta funcionando tendo que ter um token que só ela gera?
+    # o get do transfer faz o que?
+    # em um retorno da API tem "Data" e no outro esta como "data"
--- app.py
+++ app.py
@@ -1,47 +1,233 @@
-import os
-from datetime import datetime, timedelta
-import requests
-from requests.auth import HTTPBasicAuth
+from getpass import getpass
+from api_hackaton_safra import use_api
 
-pasta_script = os.path.dirname(os.path.realpath(__file__)) + '\\'
+def login():
+    exit_screen = False
+    while not exit_screen:
 
-# leitura do arquivo com token e data de expiracao
-with open( pasta_script + 'token.txt', 'r' ) as f:
-    auth_token = f.readline().replace('\n', '')
-    horario_exp_str = f.readline()
+        print( '\nTela de Inicial' )
+        print( '1 para login' )
+        print( 'enter para sair' )
+        escolha = input()
 
-horario_exp = datetime.strptime(horario_exp_str, '%Y-%m-%d %H:%M:%S.%f')
+        if escolha == '1':
+            account_id = input( 'Insira sua conta: ' )
+            password = getpass( 'Insira sua senha: ' )
 
-# caso o token expirou, renova o token
-if horario_exp <= datetime.now():
-    auth = HTTPBasicAuth('e33b611a81204f318a15d5728b998661', '31591ad2-1cbd-44e9-a01c-1bbcc0985544')
-    url = 'https://idcs-902a944ff6854c5fbe94750e48d66be5.identity.oraclecloud.com/oauth2/v1/token'
-    body = 'grant_type=client_credentials&scope=urn:opc:resource:consumer::all'
-    headers = {'Content-Type':'application/x-www-form-urlencoded'}
+            if account_id in ( '00711234511', '00711234522', '00711234533', 'teste' ) and password == '1':
+                termos()
+            else:
+                print( 'Usuário ou senha incorreto' )
+        else:
+            exit()
 
-    result = requests.post( url=url, auth=auth, data=body, headers=headers )
 
-    if result.status_code == 200:
-        auth_token = result.json()['access_token']
-        expiracao = result.json()['expires_in']
-        with open( pasta_script + 'token.txt', 'w' ) as f:
-            f.write( auth_token + '\n' )
-            f.write( str(datetime.now() + timedelta( seconds=expiracao )) )
-        print( '{0} -> Token atualizado'.format( datetime.now() ) )
-    else:
-        print( '{0} -> Erro com codigo {1}'.format( datetime.now(), result.status_code ) )
-        print( '{0} -> Erro com descricao "{1}"'.format( datetime.now(), result.json() ) )
 
-# tenta dar opt in
-auth_token = 'eyJ4NXQjUzI1NiI6IlNhWkUtSjdJdDBQWFRYNFlCaTBCeXk4WWhPVlJkSzlNNXgzREN3R2ZnbkEiLCJ4NXQiOiJVSWpBeHIyTWlzNk9JdTNMS2NsX3JPSHl3eXMiLCJraWQiOiJTSUdOSU5HX0tFWSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJlMzNiNjExYTgxMjA0ZjMxOGExNWQ1NzI4Yjk5ODY2MSIsImd0cCI6ImNjIiwidXNlci50ZW5hbnQubmFtZSI6ImlkY3MtOTAyYTk0NGZmNjg1NGM1ZmJlOTQ3NTBlNDhkNjZiZTUiLCJzdWJfbWFwcGluZ2F0dHIiOiJ1c2VyTmFtZSIsInByaW1UZW5hbnQiOnRydWUsImlzcyI6Imh0dHBzOlwvXC9pZGVudGl0eS5vcmFjbGVjbG91ZC5jb21cLyIsInRva190eXBlIjoiQVQiLCJjbGllbnRfaWQiOiJlMzNiNjExYTgxMjA0ZjMxOGExNWQ1NzI4Yjk5ODY2MSIsImNhX2d1aWQiOiJjYWNjdC1iOThlNGJjZDQ1MDU0YjZlOTc3NzU5OThiNmYzNmYwNiIsImF1ZCI6InVybjpvcGM6cmVzb3VyY2U6c2NvcGU6YWNjb3VudCIsInN1Yl90eXBlIjoiY2xpZW50Iiwic2NvcGUiOiJ1cm46b3BjOnJlc291cmNlOmNvbnN1bWVyOjphbGwiLCJjbGllbnRfdGVuYW50bmFtZSI6ImlkY3MtOTAyYTk0NGZmNjg1NGM1ZmJlOTQ3NTBlNDhkNjZiZTUiLCJleHAiOjE1OTk3ODAxMzcsImlhdCI6MTU5OTc3NjUzNywidGVuYW50X2lzcyI6Imh0dHBzOlwvXC9pZGNzLTkwMmE5NDRmZjY4NTRjNWZiZTk0NzUwZTQ4ZDY2YmU1LmlkZW50aXR5Lm9yYWNsZWNsb3VkLmNvbTo0NDMiLCJjbGllbnRfZ3VpZCI6ImMxNDYwZjZiODgxZTRhODBiMzNiYjNmYjhiMTIwYWI3IiwiY2xpZW50X25hbWUiOiJUaW1lIDIiLCJ0ZW5hbnQiOiJpZGNzLTkwMmE5NDRmZjY4NTRjNWZiZTk0NzUwZTQ4ZDY2YmU1IiwianRpIjoiMTFlYWYzYjQxNTliM2YwMjk3YWE4YjFkZmQ0NzA2NWEifQ.bgr49ZKsid3UZ1DWiStDMVCYglvOmtUFW41jdmyx4IqeW7rsk6UURdZqaeyqQPrfcsCfh_uYX-YQ5EoeCVqum1UDSBAPwF8iUnyjmLEFSd8-DLzY7OxvF38c_eSVw-x_qkJP28yVM3FQAdIwicdfeQN460dNXCvZKXwGeyP1o2SzCSIZWaH7fv03-z_OI--G4kLpLgrSyl5P_xCJP2sISIcqry1zFXl-nF8KOd5RDIoFUcquXN3doHi62gWwmj-sX5gDgJuur1H-H9n3LSa34GsloNwtxQDQ3KpyJR6DcTvtD5VSLosuykDIum_xDdRgCr3UDfpg3xXZKzNlhRUO7w'
-headers = {'Authorization': 'Bearer ' + auth_token, 'Content-Type':'application/x-www-form-urlencoded'}
-url = 'https://af3tqle6wgdocsdirzlfrq7w5m.apigateway.sa-saopaulo-1.oci.customer-oci.com/fiap-sandbox/accounts/v1/optin'
-body = {"Name": "Igão", "Email": "bongi90@hotmail.com", "Phone": "+5511991353333"}
+def termos():
 
-result = requests.post( url=url, data=body, headers=headers )
+    exit_screen = False
+    while not exit_screen:
 
-if result.status_code == 200:
-    print( '{0} -> Deu certo com resultado {1}'.format( datetime.now(), result.json() ) )
-else:
-    print( '{0} -> Erro com codigo {1}'.format( datetime.now(), result.status_code ) )
-    print( '{0} -> Erro com descricao "{1}"'.format( datetime.now(), result.json() ) )
+        print( '\nTermos' )
+        print( 'bla bla bla bla bla' )
+        print( '1 para aceitar' )
+        print( '2 para negar' )
+        escolha = input()
+
+        if escolha == '1':
+            cadastro()
+        elif escolha == '2':
+            exit()
+        else:
+            print( 'escolha inválida' )
+
+
+
+def cadastro():
+
+    print( '\nCadastro' )
+    name = input( 'Nome: ' )
+    name = input( 'E-mail: ' )
+    name = input( 'Telefone: ' )
+
+    exit_screen = False
+    while not exit_screen:
+
+        print( '1 para cadastrar' )
+        print( '2 para voltar' )
+        escolha = input()
+
+        if escolha == '1':
+            informacoes = {"Name": "Igão", "Email": "bongi90@hotmail.com", "Phone": "+5511991353333"}
+            resposta = use_api( '00711234533', 'opt_in', informacoes )
+            print( resposta )
+            perfil()
+        elif escolha == '2':
+            termos()
+        else:
+            print( 'escolha inválida' )
+
+
+
+def perfil():
+
+    print( '\nPerfil' )
+    name = input( 'pergunta 1: ' )
+
+    exit_screen = False
+    while not exit_screen:
+
+        print( '1 para avancar' )
+        print( '2 para voltar' )
+        escolha = input()
+
+        if escolha == '1':
+            objetivo()
+        elif escolha == '2':
+            cadastro()
+        else:
+            print( 'escolha inválida' )
+
+
+
+def objetivo():
+
+    print( '\nObjetivo' )
+    name = input( 'informe seu objetivo: ' )
+
+    exit_screen = False
+    while not exit_screen:
+
+        print( '1 para avancar' )
+        print( '2 para voltar' )
+        escolha = input()
+
+        if escolha == '1':
+            detalhes()
+        elif escolha == '2':
+            perfil()
+        else:
+            print( 'escolha inválida' )
+
+
+
+def detalhes():
+
+    exit_screen = False
+    while not exit_screen:
+
+        print( '\nPrincipal - Detalhes' )
+        nome = use_api( '00711234533', 'conta' )['Account']['Name']
+        print( 'Olá {0}'.format( nome ) )
+        saldo = float(use_api( '00711234533', 'saldo' ))
+        print( 'R$ {0:,.2f}'.format( saldo ) )
+        print( 'de R$ 100.000,00' )
+        print( 'faltam R$ {0:,.2f}'.format( 100000 - saldo ) )
+        print( '{0:.1f}%'.format( saldo / 1000 ) )
+        print( '...' )
+        print( '1 para Depósitos' )
+        print( '2 para Desafios' )
+        print( '3 para Adicionar saldo' )
+        print( '4 Solicitar crédito' )
+        print( '5 para sair' )
+        escolha = input()
+
+        if escolha == '1':
+            depositos()
+        elif escolha == '2':
+            desafios()
+        elif escolha == '3':
+            print('Depositos')
+        elif escolha == '4':
+            print('Depositos')
+        elif escolha == '5':
+            exit()
+        else:
+            print( 'escolha inválida' )
+
+
+
+def depositos():
+
+    exit_screen = False
+    while not exit_screen:
+
+        print( '\nPrincipal - Depósitos' )
+        print( 'Historico' )
+        print( 'Valor     Motivo         Data' )
+        extrato = use_api( '00711234533', 'extrato' )
+        for lines in extrato:
+            valor = float(lines['amount']['amount'])
+            tipo_transacao = lines['creditDebitIndicator']
+            if tipo_transacao == 'Debit':
+                print( '- R$ {0:,.2f} '.format( valor ), end='' )
+            elif tipo_transacao == 'Credit':
+                print( '+ R$ {0:,.2f} '.format( valor ), end='' )
+            print( lines['transactionInformation'], end='' )
+            print( '  {0}'.format(lines['valueDateTime']) )
+        print( '...' )
+        print( '1 para Detalhes' )
+        print( '2 para Desafios' )
+        print( '3 para sair' )
+        escolha = input()
+
+        if escolha == '1':
+            detalhes()
+        elif escolha == '2':
+            desafios()
+        elif escolha == '3':
+            exit()
+        else:
+            print( 'escolha inválida' )
+
+
+
+def desafios():
+
+    exit_screen = False
+    while not exit_screen:
+
+        print( '\nPrincipal - Desafios' )
+        print( 'Metas atingidas' )
+        print( '10% - 100 pontos' )
+        print( '...' )
+        print( '1 para Detalhes' )
+        print( '2 para Depósitos' )
+        print( '3 para Leaderboard' )
+        print( '4 para sair' )
+        escolha = input()
+
+        if escolha == '1':
+            detalhes()
+        elif escolha == '2':
+            depositos()
+        elif escolha == '3':
+            leaderboard()
+        elif escolha == '4':
+            exit()
+        else:
+            print( 'escolha inválida' )
+
+
+
+def leaderboard():
+
+    exit_screen = False
+    while not exit_screen:
+
+        print( '\nLeaderboard' )
+        print( 'Pessoa Objetivos Medalhas   Pontos' )
+        print( 'Igão A Viagem NY 8 medalhas 100.000 pontos' )
+        print( '...' )
+        print( '1 para sair' )
+        escolha = input()
+
+        if escolha == '1':
+            exit_screen = True
+        else:
+            print( 'escolha inválida' )
+
+
+
+if __name__ == '__main__':
+    login()
